cfaad99dfb7cc9bc1dda199d4134ab56567b2d20
vtimsit <victor.timsit@hetic.net>
2018-01-28 20:06:26 +0100

    Create SceneWebGl.js and update Cube.js

---
+++ app/scripts/SceneWebGl.js
@@ -0,0 +1,19 @@
+class SceneWebGl {
+    constructor() {
+
+        this.renderer = new THREE.WebGLRenderer({ canvas: document.querySelector('canvas'), antialias: false })
+
+        this.renderer.setSize(window.innerWidth, window.innerHeight)
+        this.renderer.setClearColor(0xfefefe)
+        this.renderer.setPixelRatio(window.devicePixelRatio)
+
+        this.camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 0.1, 4000)
+        this.scene = new THREE.Scene()
+
+        this.light = new THREE.AmbientLight(0xffffff, 0.5)
+        this.scene.add(this.light)
+
+        this.pointLight = new THREE.PointLight(0xffffff, 0.5)
+        this.scene.add(this.pointLight)
+    }
+}
