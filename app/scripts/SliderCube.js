6601d6c1ce34a9fcb7632099bc954409d31c1a14
vtimsit <victor.timsit@hetic.net>
2018-02-20 13:56:38 +0100

    :art: New CSS sliderCube structure

--- app/scripts/SliderCube.js
+++ app/scripts/SliderCube.js
@@ -1,17 +1,17 @@
-class SliderCube 
-{
-    constructor() 
-    {
-        this.$slides = document.querySelectorAll('.slides')
+// class SliderCube 
+// {
+//     constructor() 
+//     {
+//         this.$slides = document.querySelectorAll('.slides')
 
-        this.slidesNumber = 1
+//         this.slidesNumber = 1
 
-        document.addEventListener('mousedown', () => 
-        {
-            this.slidesNumber++
+//         document.addEventListener('mousedown', () => 
+//         {
+//             this.slidesNumber++
 
-            this.$slides[this.slidesNumber - 1].classList.remove('slides--right')
-            this.$slides[this.slidesNumber - 2].classList.add('slides--left')
-        })
-    }
-}
+//             this.$slides[this.slidesNumber - 1].classList.remove('slides--right')
+//             this.$slides[this.slidesNumber - 2].classList.add('slides--left')
+//         })
+//     }
+// }
