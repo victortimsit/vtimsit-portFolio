9b78c6dea4ac0cd11169f770020bd12960ed4373
vtimsit <victor.timsit@hetic.net>
2018-02-23 13:58:22 +0100

    :art: Remove scale of slider cubes and replace by transform origin

--- app/scripts/WavesCanvas.js
+++ app/scripts/WavesCanvas.js
@@ -12,6 +12,19 @@ class WavesCanvas {
             y: window.innerHeight / 60
         }
 
+        this.speed = {
+            one: 0,
+            two: 0,
+            three: 0,
+            four: 0,
+            five: 0,
+            six: 0,
+            seven: 0,
+            eight: 0,
+            nine: 0,
+            ten: 0
+        }
+
         this.$body.appendChild(this.$canvas)
 
         this.context.beginPath()
@@ -32,5 +45,57 @@ class WavesCanvas {
         this.context.fill()
 
         this.context.closePath()
+    }
+
+    animate() {
+        // const randomInt = (min, max) => {
+        //     return Math.random() * (max - (min)) + (min)
+        // }
+
+        // const randomSpeed = Math.random() * (1 - (-1)) + (-1)
+        
+        this.speed.one += this.randomInt(-3, 3)
+        this.speed.two += this.randomInt(-3, 3)
+        this.speed.three += this.randomInt(-3, 3)
+        this.speed.four += this.randomInt(-3, 3)
+        this.speed.five += this.randomInt(-3, 3)
+        this.speed.six += this.randomInt(-3, 3)
+        this.speed.seven += this.randomInt(-3, 3)
+        this.speed.eight += this.randomInt(-3, 3)
+        this.speed.nine += this.randomInt(-3, 3)
+        this.speed.ten += this.randomInt(-3, 3)
+
+        // console.log(randomSpeed)
+        this.clear()
+
+        this.context.beginPath()
+
+        this.gradient = this.context.createLinearGradient(0, 0, this.$canvas.width, 0)
+        this.gradient.addColorStop(0.4, '#FA709A')
+        this.gradient.addColorStop(1, '#FEE140')
+        this.context.fillStyle = this.gradient
+
+        this.context.moveTo(this.$canvas.width, this.canvasRatio.y * 4)
+        this.context.bezierCurveTo(this.canvasRatio.x * 6, this.canvasRatio.y * 4, this.canvasRatio.x * 5, this.canvasRatio.y * 12, this.canvasRatio.x * 5 + this.speed.one, this.canvasRatio.y * 24 + this.speed.two)
+        this.context.bezierCurveTo(this.canvasRatio.x * 5, this.canvasRatio.y * 36, this.canvasRatio.x * 5, this.canvasRatio.y * 47, this.canvasRatio.x * 3.5 + this.speed.three, this.canvasRatio.y * 47 + this.speed.four)
+        this.context.bezierCurveTo(this.canvasRatio.x * 2, this.canvasRatio.y * 47, this.canvasRatio.x * 1.4, this.canvasRatio.y * 58, this.canvasRatio.x * 1.4 + this.speed.five, this.canvasRatio.y * 64 + this.speed.six)
+        this.context.bezierCurveTo(this.canvasRatio.x * 1.4, this.canvasRatio.y * 70, this.canvasRatio.x * 1.6, this.canvasRatio.y * 74, this.canvasRatio.x * 1.8 + this.speed.seven, this.canvasRatio.y * 76 + this.speed.eight)
+        this.context.bezierCurveTo(this.canvasRatio.x * 2, this.canvasRatio.y * 78, this.canvasRatio.x * 2.6, this.canvasRatio.y * 83, this.$canvas.width + this.speed.nine, this.canvasRatio.y * 83 + this.speed.ten)
+        this.context.bezierCurveTo(this.$canvas.width, this.$canvas.height, this.$canvas.width, this.canvasRatio.y * 10, this.$canvas.width, this.canvasRatio.y * 4)
+
+        this.context.fill()
+
+        this.context.closePath()
+
+
+        window.requestAnimationFrame(this.animate.bind(this))
+    }
+
+    clear() {
+        this.context.clearRect(0, 0, this.$canvas.width, this.$canvas.height)
+    }
+
+    randomInt(min, max) {
+        return Math.random() * (max - (min)) + (min)
     }
 }
--- app/scripts/script.js
+++ app/scripts/script.js
@@ -5,7 +5,6 @@
 
 const wavesCanvas = new WavesCanvas(window.innerWidth, window.innerHeight * 2)
 
-const slider = new SliderCube('.cubeSlider_1__item', 300, 300, 90, 0)
+const slider = new SliderCube('.cubeSlider_1__item', 580, 580, 90, 0)
 const slider_2 = new SliderCube('.cubeSlider_2__item', 220, 220, 0, -90)
 const slider_3 = new SliderCube('.cubeSlider_3__item', 98, 98, -90, 0)
-const slider_4 = new SliderCube('.cubeSlider_4__item', 220, 220, 0, 90)
