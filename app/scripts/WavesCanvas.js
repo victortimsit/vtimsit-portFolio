7f755ef1411d26cdf9567fa3baf97e7a349bd3c6
vtimsit <victor.timsit@hetic.net>
2018-02-23 12:06:15 +0100

    :art: Add cubicBezier curves

--- app/scripts/WavesCanvas.js
+++ app/scripts/WavesCanvas.js
@@ -5,9 +5,23 @@ class WavesCanvas {
         this.context = this.$canvas.getContext('2d')
 
         this.$canvas.width = width
-		this.$canvas.height = height
+        this.$canvas.height = height
+        
+        this.canvasRatio = {
+            x: window.innerWidth / 8,
+            y: window.innerHeight / 60
+        }
 
         this.$body.appendChild(this.$canvas)
 
+        this.context.beginPath()
+        this.context.moveTo(this.$canvas.width, this.canvasRatio.y * 4)
+        this.context.bezierCurveTo(this.canvasRatio.x * 6, this.canvasRatio.y * 4, this.canvasRatio.x * 5, this.canvasRatio.y * 12, this.canvasRatio.x * 5, this.canvasRatio.y * 24)
+        this.context.bezierCurveTo(this.canvasRatio.x * 5, this.canvasRatio.y * 36, this.canvasRatio.x * 5, this.canvasRatio.y * 47, this.canvasRatio.x * 3.5, this.canvasRatio.y * 47)
+        this.context.bezierCurveTo(this.canvasRatio.x * 2, this.canvasRatio.y * 47, this.canvasRatio.x * 1.4, this.canvasRatio.y * 58, this.canvasRatio.x * 1.4, this.canvasRatio.y * 64)
+        this.context.bezierCurveTo(this.canvasRatio.x * 1.4, this.canvasRatio.y * 70, this.canvasRatio.x * 1.6, this.canvasRatio.y * 74, this.canvasRatio.x * 1.8, this.canvasRatio.y * 76)
+        this.context.bezierCurveTo(this.canvasRatio.x * 2, this.canvasRatio.y * 78, this.canvasRatio.x * 2.6, this.canvasRatio.y * 83, this.$canvas.width, this.canvasRatio.y * 83)
+        this.context.bezierCurveTo(this.$canvas.width, this.$canvas.height, this.$canvas.width, this.canvasRatio.y * 10, this.$canvas.width, this.canvasRatio.y * 4)
+        this.context.stroke()
     }
 }
--- app/scripts/script.js
+++ app/scripts/script.js
@@ -3,7 +3,7 @@
 // const cursorParallax = new CursorParallax(cube)
 // cube.animate()
 
-const wavesCanvas = new WavesCanvas(window.innerWidth, window.innerHeight)
+const wavesCanvas = new WavesCanvas(window.innerWidth, window.innerHeight * 2)
 
 const slider = new SliderCube('.cubeSlider_1__item', 300, 300, 90, 0)
 const slider_2 = new SliderCube('.cubeSlider_2__item', 220, 220, 0, -90)
