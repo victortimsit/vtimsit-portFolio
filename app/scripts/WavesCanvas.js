029144f2b7396f73bd4b4eb42584becc8ffa8319
vtimsit <victor.timsit@hetic.net>
2018-02-23 12:13:55 +0100

    :art: Add linear gradient on canvas waves

--- app/scripts/WavesCanvas.js
+++ app/scripts/WavesCanvas.js
@@ -15,6 +15,12 @@ class WavesCanvas {
         this.$body.appendChild(this.$canvas)
 
         this.context.beginPath()
+
+        this.gradient = this.context.createLinearGradient(0, 0, this.$canvas.width, 0)
+        this.gradient.addColorStop(0.4, '#FA709A')
+        this.gradient.addColorStop(1, '#FEE140')
+        this.context.fillStyle = this.gradient
+
         this.context.moveTo(this.$canvas.width, this.canvasRatio.y * 4)
         this.context.bezierCurveTo(this.canvasRatio.x * 6, this.canvasRatio.y * 4, this.canvasRatio.x * 5, this.canvasRatio.y * 12, this.canvasRatio.x * 5, this.canvasRatio.y * 24)
         this.context.bezierCurveTo(this.canvasRatio.x * 5, this.canvasRatio.y * 36, this.canvasRatio.x * 5, this.canvasRatio.y * 47, this.canvasRatio.x * 3.5, this.canvasRatio.y * 47)
@@ -22,6 +28,9 @@ class WavesCanvas {
         this.context.bezierCurveTo(this.canvasRatio.x * 1.4, this.canvasRatio.y * 70, this.canvasRatio.x * 1.6, this.canvasRatio.y * 74, this.canvasRatio.x * 1.8, this.canvasRatio.y * 76)
         this.context.bezierCurveTo(this.canvasRatio.x * 2, this.canvasRatio.y * 78, this.canvasRatio.x * 2.6, this.canvasRatio.y * 83, this.$canvas.width, this.canvasRatio.y * 83)
         this.context.bezierCurveTo(this.$canvas.width, this.$canvas.height, this.$canvas.width, this.canvasRatio.y * 10, this.$canvas.width, this.canvasRatio.y * 4)
-        this.context.stroke()
+
+        this.context.fill()
+
+        this.context.closePath()
     }
 }
