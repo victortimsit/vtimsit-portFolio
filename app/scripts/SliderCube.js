848f98f06a9aab068ac9f93b8c56af1a4e8febb1
vtimsit <victor.timsit@hetic.net>
2018-02-28 16:58:47 +0100

    :lipstick: Translate sliderCube width and height in px

--- app/scripts/SliderCube.js
+++ app/scripts/SliderCube.js
@@ -21,12 +21,6 @@ class SliderCube {
 
         // Add slide style
         for (const $slide of this.$slides) {
-            // $slide.style.width = `${this.slideWidth}vw` // Set slide width
-            // $slide.style.height = `${this.slideHeight}vw` // Set slide height
-
-            // $slide.style.left = `${- this.slideWidth / 2}px` // Set slide left
-            // $slide.style.top = `${- this.slideHeight / 2}px` // Set slide top
-
             $slide.style.opacity = 0
             $slide.style.transformOrigin = `50% 50% ${-$slide.offsetWidth / 2}px`
             $slide.style.transform = `rotateY(${this.rotation.yPos}deg)  rotateX(${this.rotation.xPos}deg)` // Set slide transformations
